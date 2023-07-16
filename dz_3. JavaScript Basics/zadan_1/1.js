830ab1d0ff98d25dab2da926693d0b427e0be678
ShamaicH <maga74518@mail.ru>
2023-07-16 10:24:14 +0300

    подтянул остальные файлы

--- dz_3. JavaScript Basics/zadan_1/1.js
+++ dz_3. JavaScript Basics/zadan_1/1.js
@@ -1,4 +1,4 @@
-const temp = parseInt(prompt('Введите температуру в градусах цельсия: '))
-let temp1 = (9 / 5) * temp + 32
+const temp = parseInt(prompt('Введите температуру в градусах цельсия: '));
+let temp1 = (9 / 5) * temp + 32;
 
-alert(`Цельсий: ${temp}, Фаренгейт: ${temp1.toFixed(1)}`)
+alert(`Цельсий: ${temp}, Фаренгейт: ${temp1.toFixed(1)}`);
